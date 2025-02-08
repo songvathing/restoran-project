d13f3e17d78c5aee9845fafbd52eff997771c4c2
Hing Songvat <hings975@gmail.com>
2025-02-08 13:37:25 +0700

    setup arrow-up

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 
+$(".arrowup").on("click", function () {
+  $("html, body").animate(
+    {
+      scrollTop: $("#home").offset().top,
+    },
+    "smooth",
+    function () {
+      $(".arrowup").hide();
+    }
+  );
+});
+$(window).on("scroll", function () {
+  if ($(window).scrollTop() > 100) {
+    $(".arrowup").show();
+  } else {
+    $(".arrowup").hide();
+  }
+});
